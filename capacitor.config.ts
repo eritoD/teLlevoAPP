9122b07f78ce2a9665f099380e6595e41224691b
ToroDanger <bryanpmg90@gmail.com>
2024-08-29 21:07:21 -0400

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'teLlevoAPP',
+  webDir: 'www'
+};
+
+export default config;
