55036de649da307cc10d574f6eaf8fea8a3530cf
eritoD <eri.diaz@duocuc.cl>
2024-08-31 08:47:57 -0400

    aa

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,29 @@
 import { Component } from '@angular/core';
+import { Animation,AnimationController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
 
-  constructor() {}
+export class HomePage {
+  private animation:Animation 
+  constructor(private aCrl:AnimationController) {}
+
+  ngOnIt(){}//se ejecuta al inicio la pagina
+  ngAfterViewInit(){//inmediatamente despues de ngOnDestroy
+    this.animation = this.aCrl.create()
+    .addElement(document.querySelector('.square') as HTMLElement) //el elemento que voy a referenciar
+    .duration(1500) //cuanto dura la animacion en ms
+    .interations(2) //la cantidad de veces que se ejecuta la animacion
+    .fromTo('','','')//animacion
+  
+  
+  } 
+  ngOnDestroy(){}//se ejecuta al cerrar la pagina
+  
+
+
 
 }
